ba36bfdf6b25942d6ed60cb57732f1346ad25b33
Shivam Jain <shivam.jain@muoro.com>
2025-01-15 15:50:55 +0530

    update vercel json file

--- sample/urls.py
+++ sample/urls.py
@@ -1,10 +1,9 @@
 # example/urls.py
 from django.urls import path
 
-from sample import views
 from sample.views import index
 
 
 urlpatterns = [
-    path('', views.hello),
+    path('', index),
 ]
--- sample/views.py
+++ sample/views.py
@@ -6,11 +6,6 @@ from datetime import datetime
 
 from django.http import HttpResponse
 
-
-def hello(request):
-    return HttpResponse("Hello, World!")
-
-
 def index(request):
     now = datetime.now()
     html = f'''
