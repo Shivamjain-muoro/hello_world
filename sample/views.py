bd873ebc7a690ff71a93954e8626519b10d50f8b
Shivam Jain <shivam.jain@muoro.com>
2025-01-15 13:38:18 +0530

    update view with Hello World Django project

--- sample/views.py
+++ sample/views.py
@@ -12,7 +12,7 @@ def index(request):
     html = f'''
     <html>
         <body>
-            <h1>Hello from Vercel!</h1>
+            <h1>Hello from Django Project!</h1>
             <p>The current time is { now }.</p>
         </body>
     </html>
